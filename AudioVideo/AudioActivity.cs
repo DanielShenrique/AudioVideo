78fa4bb4d52d651eb656e7e902dda44d9ac3ae75
Daniel Henrique <danielshenrique@gmail.com>
2018-10-30 23:46:56 -0300

    1
    1 - começo do trabalho

---
+++ AudioVideo/AudioActivity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AudioVideo
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+	class AudioActivity: Activity
+	{
+		private MediaPlayer mp;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_audio);
+
+			mp = MediaPlayer.Create(this, Resource.Raw.StarWars);
+
+			//Button startAudio = (Button)FindViewById(Resource.Id);
+		}
+
+		private void ButtonPlay(object sender, System.EventArgs e)
+		{
+			if(mp != null && !mp.IsPlaying)
+			{
+				mp = MediaPlayer.Create(this, Resource.Raw.StarWars);
+				mp.Start();
+			}
+		}
+		private void ButtonStop(object sender, System.EventArgs e)
+		{
+			MPManager(1);
+		}
+
+		#region States
+		protected override void OnPause()
+		{
+			base.OnPause();
+
+			MPManager(0);
+		}
+		protected override void OnStop()
+		{
+			base.OnStop();
+
+			MPManager(1);
+		}
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+
+			MPManager(2);
+		}
+		#endregion
+
+		private void MPManager(int state)
+		{
+			if (mp != null && mp.IsPlaying)
+			{
+				switch (state)
+				{
+					case 0: mp.Pause(); break;
+					case 1: mp.Stop(); break;
+					case 2: mp.Release(); break;
+				}
+			}
+		}
+	}
+}
---
+++ AudioVideo/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace AudioVideo
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+			Button audio = (Button)FindViewById(Resource.Id.Audio);
+			Button video = (Button)FindViewById(Resource.Id.Video);
+
+			audio.Click += GoToTheAudio;
+			video.Click += GoToTheVideo;
+        }
+
+		void GoToTheAudio(object sender, System.EventArgs e)
+		{
+			Intent i = new Intent(this, typeof(AudioActivity));
+			this.StartActivity(i);
+		}
+		void GoToTheVideo(object sender, System.EventArgs e)
+		{
+			Intent i = new Intent(this, typeof(VideoActivity));
+			this.StartActivity(i);
+		}
+    }
+}
+
